161de3a82744edf82c2c9c080fb9ad134ea2446a
msevillano <sevillano.mauro@gmail.com>
2019-07-22 11:47:40 -0300

    add winston as logger and enforce the use of it

---
+++ src/core/logger.js
@@ -0,0 +1,41 @@
+import winston from 'winston';
+const colorize = winston.format.colorize();
+
+const logConfig = {
+  levels: {
+    debug: 5,
+    info: 4,
+    warn: 3,
+    fail: 2,
+    error: 1,
+    fatal: 0,
+  },
+  colors: {
+    debug: 'blue',
+    info: 'green',
+    warn: 'yellow',
+    fail: 'orange',
+    error: 'red',
+    fatal: 'magenta',
+  },
+};
+
+winston.addColors(logConfig.colors);
+
+export default winston.createLogger({
+  levels: logConfig.levels,
+  format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf((msg) =>
+        colorize.colorize(msg.level, `${msg.timestamp} - ${msg.level}: ${msg.message}`)
+      )
+  ),
+  transports: [
+    new winston.transports.Console( {
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+    },),
+  ],
+});
